9186f6906a39fad8f95c5deb41ff6772c3623ffd
felipecalderero <felipecalderero@gmail.com>
2024-04-26 15:22:07 +0200

    student without cohort solved

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -20,7 +20,6 @@ function SignupPage() {
     e.preventDefault();
     // Create an object representing the request body
     const requestBody = { email, password, name };
-
     // Make an axios request to the API
     // If POST request is successful redirect to login page
     // If the request resolves with an error, set the error message in the state
